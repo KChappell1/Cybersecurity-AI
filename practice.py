682aec2303eb0eec834ffd0d9c89253bab15a703
Kristian Chappell <kchappell1@cps.edum>
2024-01-22 11:31:42 -0600

    worked on ai

--- practice.py
+++ practice.py
@@ -1,6 +1,21 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-dataset = pd.read_csv('Phishing_Legitimate_full.csv')
 
- x = dataset.drop
+import numpy as np
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Practice_Dataset_iris.data.csv')
+
+x = dataset.drop(columns=["Species"]).values
+y = dataset["Species"].values
+# print(y)
+
+#Standardizes the x values
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+print(x_train)
