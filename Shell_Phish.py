e0689a29df89569c16c0968579bd231f2d48f135
KChappell1 <143532946+KChappell1@users.noreply.github.com>
2024-01-25 10:29:08 -0600

    Started Shell_Phish.py

---
+++ Shell_Phish.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Phishing_Legitimate_full')
+
+x = dataset.drop(columns=["id", "CLASS_LABEL"]).values
+y = y = dataset["CLASS_LABEL"].values
+
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+model = linear_model.LogisticRegression().fit(x_train, y_train)
+
+print("Accuracy:", model.score(x_test, y_test))
