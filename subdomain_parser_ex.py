d509d0b4417ece6939021fe677ae59114e9eb435
Kristian Chappell <kchappell1@cps.edum>
2024-02-13 11:28:43 -0600

    renamed ssss

--- subdomain_parser_ex.py
+++ subdomain_parser_ex.py
@@ -4,7 +4,7 @@ def extract_levels(url):
     ext = tldextract.extract(url)
     return ext.subdomain, ext.domain, ext.suffix
 
-url = "https://subdomain.example.co.uk/path/to/page"
+url = "https://google.google.com"
 subdomain, domain, suffix = extract_levels(url)
 print("Subdomain:", subdomain)
 print("Domain:", domain)
